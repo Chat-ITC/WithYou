a5080a9278f53c14784d946bb27327da215c3835
hong <kevin0928@naver.com>
2023-10-29 01:09:48 +0900

    docs: 로그 추가

--- src/main/java/WithYou/domain/scrap/controller/ScrapController.java
+++ src/main/java/WithYou/domain/scrap/controller/ScrapController.java
@@ -4,6 +4,7 @@ import WithYou.domain.ai.dto.response.QuestionResponseDto;
 import WithYou.domain.scrap.service.ScrapService;
 import WithYou.global.jwt.MemberPrincipal;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ScrapController {
     private final ScrapService scrapService;
 
     @GetMapping("/scrap/{id}")
     public ResponseEntity<?> scrapHistory(@AuthenticationPrincipal MemberPrincipal memberPrincipal,
                                           @PathVariable Long id) {
+        log.info(id.toString());
         QuestionResponseDto questionResponseDto = scrapService.scrapContent(id);
+        log.info("여기까진 성공1");
         scrapService.saveSummaryContent(questionResponseDto, memberPrincipal.getMember());
+        log.info("여기까진 성공2");
         return ResponseEntity.ok()
                 .body(questionResponseDto.getIsScrap());
     }
--- src/main/java/WithYou/domain/scrap/service/ScrapService.java
+++ src/main/java/WithYou/domain/scrap/service/ScrapService.java
@@ -7,23 +7,28 @@ import WithYou.domain.ai.repository.AiRepository;
 import WithYou.domain.member.entity.Member;
 import WithYou.domain.scrap.exception.ContentNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ScrapService {
     private AiRepository aiRepository;
 
     public QuestionResponseDto scrapContent(Long id) {
         AiSummaryContent aiSummaryContent = getAiSummaryContent(id);
         aiSummaryContent.setIsScrap(aiSummaryContent.getIsScrap() == IsScrap.No ? IsScrap.YES : IsScrap.No);
+        log.info(aiSummaryContent.getIsScrap().toString());
         return QuestionResponseDto.of(aiSummaryContent);
     }
 
     private AiSummaryContent getAiSummaryContent(Long id) {
         AiSummaryContent aiSummaryContent = aiRepository.findAiSummaryContentById(id)
                 .orElseThrow(() -> new ContentNotFoundException());
+        log.info("aiSummaryContent 불러오기 성공");
+        log.info(aiSummaryContent.getContent());
         return aiSummaryContent;
     }
 
