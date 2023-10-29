8ce55c2ee5d447608998343a26e95552b91a04a4
Wonbin Choi <dnjsqls5973@naver.com>
2023-10-29 16:08:11 +0900

    fix: Chat gpt 역할 수정

--- src/main/java/WithYou/domain/ai/dto/request/ChatGptRequestDto.java
+++ src/main/java/WithYou/domain/ai/dto/request/ChatGptRequestDto.java
@@ -10,10 +10,10 @@ public class ChatGptRequestDto {
     private List<Message> messages;
     private int max_tokens;
 
-    public ChatGptRequestDto(String model, String content) {
+    public ChatGptRequestDto(String model, String content) {        //매개변수에 학생 학과를 받아와서 사용하면 될듯
         this.model = model;
         this.messages = new ArrayList<>();
-        this.messages.add(new Message("system", "너는 유능한 IT 개발자야. IT에 관한 지식이 풍부하며 IT 관련 질문에 반드시 답할 수 있어."));
+        this.messages.add(new Message("system", "너는 대학교 교수야. 전공 관련 질문에 대해서 항상 정확한 답변을 주며 어떤 질문이든 반드시 대답할 수 있어."));
         //@authenticationprincipal을 이용해서 User의 학과를 불러온 후 해당 학과 교수로서 답을 하도록 역할 부여할 예정
         this.messages.add(new Message("user", content));
         this.max_tokens = 800;
