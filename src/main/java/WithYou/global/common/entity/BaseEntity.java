9dd54c4d55aeb0bdd5063767c05d18d6af660a60
hong <kevin0928@naver.com>
2023-10-31 16:47:39 +0900

    fix: LocalDateTime을 Asia/Seoul로 설정

--- src/main/java/WithYou/global/common/entity/BaseEntity.java
+++ src/main/java/WithYou/global/common/entity/BaseEntity.java
@@ -1,8 +1,10 @@
 package WithYou.global.common.entity;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -12,8 +14,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+    @Transient
+    ZoneId seoulZone = ZoneId.of("Asia/Seoul"); // ZoneId가 칼럼이 되는 것을 방지
     @CreatedDate
-    private LocalDateTime createdDate = LocalDateTime.now();
+    private LocalDateTime createdDate = LocalDateTime.now(seoulZone);
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
 }
