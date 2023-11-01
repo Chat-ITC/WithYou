235e30e8a90421506309c28b197926f41a125feb
hong <kevin0928@naver.com>
2023-11-02 00:44:23 +0900

    feat: 로그아웃 구현 완료

--- src/main/java/WithYou/domain/scrap/repository/ScrapRepository.java
+++ src/main/java/WithYou/domain/scrap/repository/ScrapRepository.java
@@ -1,12 +1,9 @@
 package WithYou.domain.scrap.repository;
 
 import WithYou.domain.scrap.entity.Scrap;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
-    List<Scrap> findScrapByMemberIdOOrderByIdDesc(Long memberId);
-
     Optional<Scrap> findScrapByPostId(Long postId);
 }
