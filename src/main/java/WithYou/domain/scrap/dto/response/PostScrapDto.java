136f963794686b37e440f87fa2ab72197f2dce2b
Wonbin Choi <dnjsqls5973@naver.com>
2023-11-01 14:36:19 +0900

    fix: PostScrapDto에 Getter 추가

--- src/main/java/WithYou/domain/scrap/dto/response/PostScrapDto.java
+++ src/main/java/WithYou/domain/scrap/dto/response/PostScrapDto.java
@@ -2,10 +2,13 @@ package WithYou.domain.scrap.dto.response;
 
 import WithYou.domain.scrap.entity.Scrap;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
+@Getter
 public class PostScrapDto {
     private Long postId;
     private String title;
