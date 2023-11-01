c9f6e06953a1fcd5b5f6874ad7d69e0c47819caa
hong <kevin0928@naver.com>
2023-11-02 00:06:13 +0900

    fix: findAiSummaryContentList에도 orderBy 추가

--- src/main/java/WithYou/domain/ai/repository/AiQueryRepository.java
+++ src/main/java/WithYou/domain/ai/repository/AiQueryRepository.java
@@ -23,6 +23,7 @@ public class AiQueryRepository {
     public Page<AiSummaryContent> findAiSummaryContentList(Pageable pageable, Member member) {
         JPAQuery<AiSummaryContent> query = jpaQueryFactory
                 .selectFrom(aiSummaryContent) // 별칭 설정
+                .orderBy(aiSummaryContent.id.desc())
                 .where(aiSummaryContent.member.eq(member))
                 .offset(pageable.getOffset()) // 페이지네이션 설정
                 .limit(pageable.getPageSize());
